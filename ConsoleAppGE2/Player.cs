d69abffaef50c5734c1e33b4d7cb5e1c6633ff10
S-JOBI23-054\student <231076@st.yoshida.-g.ac.jp>
2024-06-14 10:21:49 +0900

    任意のプレイヤー人数登録、表示対応 #4 #5

--- ConsoleAppGE2/Player.cs
+++ ConsoleAppGE2/Player.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.CompilerServices;
@@ -7,27 +8,77 @@ using System.Threading.Tasks;
 
 namespace ConsoleAppGE2
 {
-     class Player
+    class Player
     {
-        private string name = "";
-        private string job = "";
 
-        public void CreatePlayer(string name, string job)
+        private bool i = true;
+        private string name = "";
+        private int attack = 0;
+        private int defense = 0;
+       
+        /// <summary>
+        /// ステータス入力処理
+        /// </summary>
+        public void CreatePlayer()
         {
-            this.name = name;
-            this.job = job;
+
+            Console.Write("\n名前を入力＞ ");  // 文字列を表示
+            this.name = Console.ReadLine();     // 名前の入力
+
+            /// 攻撃力入力
+            while (i == true)
+            {
+
+                Console.Write("攻撃力を入力＞");
+                if (int.TryParse(Console.ReadLine(), out attack))
+                {
+                    i = false;
+                }
+                else if (attack > 100 || attack <= 0)
+                {
+                    Console.WriteLine("入力された数値が上限より上、または認識されませんでした");
+
+
+                }
+
+            }
+            ///防御力入力
+            while (i == false)
+            {
+                Console.Write("防御を入力＞");
+                if (int.TryParse(Console.ReadLine(), out defense))
+                {
+                    i = true;
+                }
+                else if (defense > 100 || defense <= 0)
+                {
+                    Console.WriteLine("入力された数値が上限より上、または認識されませんでした");
+
+
+                }
+                else
+                {
+                    Console.WriteLine("入力された数値が上限より上、または認識されませんでした");
+
+
+                }
+            }
+            Console.Clear();
 
         }
-
+        /// <summary>        
+        /// 出力処理
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="defense"></param>
+        /// <param name="luck"></param>
         public void ShowPlayer()
         {
-            Console.WriteLine("名前:{0} 役職: {1}",name,job);
 
+            Console.WriteLine("名前：{0}, 攻撃力：{1}, 防御力：{2}\n", name, attack, defense);
         }
 
     }
-
-    
-
-    
 }
+
+
--- ConsoleAppGE2/Program.cs
+++ ConsoleAppGE2/Program.cs
@@ -1,17 +1,90 @@
 ﻿using System;
-using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using System.Text.Json;
-using System.Xml.Linq;
 
 namespace ConsoleAppGE2
 {
-    public static class Function1
+    class Program
     {
-        static Player player = new Player();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="パーティ情報"></param>
+        static void Main(string[] args)
+        {
+            ///ループ用変数          
+            int loop = 0;
 
-      
+            ///プレイヤー人数用変数           
+            int PlayNum = 0;
 
+            Console.Clear();
+
+            ///プレイヤー人数設定
+            while (loop == 0)
+            {
+                Console.Write("入力するプレイヤーの人数を入力してください＞");
+                if (int.TryParse(Console.ReadLine(), out PlayNum))
+                {
+                    ///範囲内の人数
+                    if (PlayNum <= 4 && PlayNum > 0)
+                    {
+
+                        ///ループを抜ける
+                        loop++;
+                    }
+                    ///範囲外の人数
+                    else
+                    {
+                        Console.WriteLine("入力された数値が上限より上、または認識されませんでした");
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("入力された数値が上限より上、または認識されませんでした");
+
+                }
+            }
+
+            Console.Clear();
+
+            // 配列を作成
+            Player[] players = new Player[PlayNum];
+
+            // インスタンス生成
+            for (loop = 0; loop < PlayNum; loop++)
+            {// プレイヤー人数分ループ
+                players[loop] = new Player();
+            }
+
+            // ステータス入力
+            for (loop = 0; loop < PlayNum; loop++)
+            {
+
+                Console.Write(loop + 1);
+                Console.WriteLine("人目のステータスを入力");
+
+                players[loop].CreatePlayer();
+
+            }
+
+            ///プレイヤー情報出力
+            Console.Write("\nプレイヤー情報\n\n ");  // 文字列を表示
+            for (loop = 0; loop < PlayNum; loop++)
+            {
+                //プレイヤー人数分ループ
+                Console.Write("{0}人目\n", loop + 1);  // 文字列を表示
+
+                // メソッド：出力
+                players[loop].ShowPlayer();
+            }
+
+            Console.Write("\nエンターを押して閉じる");
+            Console.ReadLine();
+
+        }
     }
 }
-
